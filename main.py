29b6fd3c9c0cd991c52b7d801a941dd1feb4b77d
Rakugen <simonlee0306@gmail.com>
2023-02-28 22:29:08 -0500

    Create Tkinter app, canvas, text label and button

--- main.py
+++ main.py
@@ -7,4 +7,78 @@
 # Use case: e.g. you want to start posting your photos to Instagram, but you want to add your website to all
 # the photos, you can now use your software to add your website/logo automatically to any image.
 
+# ToDo: ACTIONABLE PLAN:
+# 1. create a box with a button
+# 2. create a form/dropdown to select an image file
+# 3. display selected image
+# 4. create an input w/ button for watermark text
+# 5. can move around/determine where the watermark will go
+# 6. clicking a button will save the image/ask to save w/watermark
+# optional: allow resizing of watermark, allow changing of opacity of watermark.
+
+
+import tkinter.filedialog
+from tkinter import *
+from PIL import Image, ImageFont, ImageDraw, ImageTk
+
+
+BG_COLOR = "#B1DDC6"
+FG_COLOR = "#FF1234"
+FONT = "Arial"
+
+
+image = Image.open("shiba.jpg")
+# watermark_image = image.copy()
+# draw = ImageDraw.Draw(watermark_image)
+# # ("font type",font size)
+# w, h = image.size
+# x, y = int(w / 2), int(h / 2)
+# if x > y:
+#     font_size = y
+# elif y > x:
+#     font_size = x
+# else:
+#     font_size = x
+# font = ImageFont.truetype("arial.ttf", int(font_size / 6))
+
+# add Watermark
+# (0,0,0)-black color text
+# draw.text((x, y), "puppy", fill=(0, 0, 0), font=font, anchor='ms')
+
+
+def select_file(canvas):
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+    filename = tkinter.filedialog.askopenfilename(
+        title="OPEN A FILE",
+        initialdir='/',
+        filetypes=filetypes
+    )
+    print(filename)
+    canvas.itemconfig(canvas_image, image=ImageTk.PhotoImage(Image.open(filename)))
+    # Image.open(filename).show()
+
+
+window = Tk()
+window.title("Watermark Editor")
+window.config(padx=50, pady=50, bg=BG_COLOR)
+
+canvas = Canvas(width=500, height=600, highlightthickness=0)
+shiba_img = ImageTk.PhotoImage(Image.open("shiba.jpg").resize((500, 600)))
+# resize_shiba = Image.open("new_img1.png").resize((400,400))
+# canvas.create_image(100, 300, anchor="nw", image=shiba_img)
+
+text_label = Label(text="Text", fg=FG_COLOR, font=(FONT, 16))
+text_label.grid(row=1)
+
+button1 = Button(text="Button1", command=lambda: select_file(canvas))
+button1.grid(row=2, pady=10)
+canvas_image = canvas.create_image(250, 300, image=shiba_img)
+canvas.grid(row=0, pady=10)
+
+
+window.mainloop()
+
 
