d01a0bf2df42c8c5a3fe5c77e24e27e04d9071d7
Rakugen <simonlee0306@gmail.com>
2023-03-02 06:16:02 -0500

    add instructions label

--- main.py
+++ main.py
@@ -76,20 +76,16 @@ def write_watermark(text):
 def rescale_img(image):
     copy = image.copy()
     copy.thumbnail(size=(BASE_WIDTH, BASE_HEIGHT), resample=Image.LANCZOS)
-    # wpercent = (BASE_WIDTH / float(image.size[0]))
-    # hsize = int((float(image.size[1]) * float(wpercent)))
-    # hpercent = (BASE_HEIGHT / float(image.size[1]))
-    # wsize = int((float(image.size[0]) * float(hpercent)))
-    # return image.resize((BASE_WIDTH, hsize))
     return copy
 
 def clicked(event):
-    print(f'clicked at {event.x},{event.y}')
+    print(f'clicked at {event.x},{event.y-34}')
 
 # =================================== UI SETUP ============================================
 
 window = Tk()
 window.title("Watermark Editor")
+# window.geometry("750x750")
 window.config(padx=15, bg=BG_COLOR)
 canvas = Canvas(width=BASE_WIDTH, height=BASE_HEIGHT, highlightthickness=0, bg=BG_COLOR)
 
@@ -98,8 +94,14 @@ original_img = Image.open('shiba.jpg')
 shiba_img = ImageTk.PhotoImage(rescale_img(original_img))
 
 # Labels
-text_label = Label(text="Text", fg=FG_COLOR, font=(FONT, 16))
-text_label.grid(row=1)
+img_name_label = Label(text="Open Image Here", fg=FG_COLOR, font=(FONT, 10))
+img_name_label.grid(row=1)
+instruction_label = Label(
+    text='First open an image, then enter \nwatermark text and click on image \nPress button to write',
+    font=(FONT,10),
+    bg=BG_COLOR
+    )
+instruction_label.grid(row=1, column=1, columnspan=2, rowspan=2)
 
 # Buttons
 button1 = Button(text="Open Image", command=lambda: select_file(canvas))
@@ -113,12 +115,14 @@ canvas.grid(row=0, columnspan=4, pady=15)
 
 # Tkinter Entry widget to input watermark text
 wm_text = tkinter.StringVar()
-entry1 = Entry(window, textvariable=wm_text, font=('arial',10,'normal'))
+entry1 = Entry(window, textvariable=wm_text, font=(FONT, 10))
 entry1.insert(0, "Input Text Here")
 entry1.grid(row=1, column=3)
 
 # TODO: Test a click event option
-canvas.bind("<Button-1>", clicked)
+#  Need to adjust click event to be relative to image coordinates and not window
+canvas.tag_bind(canvas_image, "<Button-1>", clicked)
+
 
 
 # TODO: ComboBox widget to select where to place watermark OR 2 scale widgets for x + y coords
